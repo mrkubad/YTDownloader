53d9c729cf066dafabe7b32da4f437d83d3dcd4e
MrKubaD <jakub.dedio@microsoft.wsei.edu.pl>
2020-02-07 15:28:34 +0100

    added Downloader class

---
+++ YTDownloaderCore/Downloader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace YTDownloaderCore
+{
+    class Downloader : WebClient
+    {
+        private readonly string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299";
+        public Downloader()
+        {
+            Headers.Add("User-Agent", UserAgent);
+        }
+        public bool FastDashDownload { get; set; }
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            HttpWebRequest request = (HttpWebRequest)base.GetWebRequest(address);
+            if (FastDashDownload)
+                request.AddRange(0);
+            return request;
+        }
+
+    }
+}
