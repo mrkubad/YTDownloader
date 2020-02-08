12e2ef9cf7061be733831f855b8980c35a0f931a
MrKubaD <jakub.dedio@microsoft.wsei.edu.pl>
2020-02-08 17:15:03 +0100

    created provisional look and added some logic

--- YTDownloaderApp/MainWindow.xaml.cs
+++ YTDownloaderApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using YTDownloaderApp.Controls;
+using YTDownloaderCore.Regexes;
 
 namespace YTDownloaderApp
 {
@@ -20,9 +23,42 @@ namespace YTDownloaderApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ICommand WindowMouseMove { get; set; }
+        public ICommand ListViewDoubleClick { get; set; }
+        public ICommand ListViewSelectionChanged { get; set; }
+
+        private string PreviousValidUrl;
         public MainWindow()
         {
             InitializeComponent();
+            WindowMouseMove = new RelayCommand(o =>
+            {
+                if(Clipboard.ContainsText())
+                {
+                    string clipboardContent = Clipboard.GetText();
+                    if(RegexLibrary.YoutubeUrl.IsMatch(clipboardContent))
+                    {
+                        if (string.Compare(PreviousValidUrl, clipboardContent) != 0)
+                        {
+                            PreviousValidUrl = clipboardContent;
+
+                            // TODO: We need to create object here representing item on LV
+                        }
+                    }
+                }
+            });
+            ListViewDoubleClick = new RelayCommand(o => {
+                Debug.WriteLine(o);
+                Debug.WriteLine("Działa z komanda!!");
+            
+            });
+            ListViewSelectionChanged = new RelayCommand(o =>
+            {
+                Debug.WriteLine(o);
+                Debug.WriteLine("Command SelectionChanged");
+            });
+            DataContext = this;
+            lv.ItemsSource = new List<string> { "dupa", "sraka", "3", "4", "5", "6", "7" };
         }
     }
 }
---
+++ YTDownloaderCore/Regexes/RegexLibrary.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+namespace YTDownloaderCore.Regexes
+{
+    public static class RegexLibrary
+    {
+        public static Regex YoutubeUrl { get; } = new Regex("http(?:s?):\\/\\/(?:www\\.)?youtu(?:be\\.com\\/watch\\?v=)(?<VideoId>[^&^\\s]+)(?:.*?&list=(?<ListId>[^&^\\s]+))?", System.Text.RegularExpressions.RegexOptions.Compiled);
+    }
+}
